c5e6bb909a24659f2ec129a37a1d2fd7c1266977
aguss rodriguez <agussrg80@gmail.com>
2022-06-09 21:15:54 -0300

    firstPart

---
+++ app.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.button')
+const links = document.querySelector('.nav__links')
+
+
+button.addEventListener('click', () => {
+    links.classList.toggle('active')
+})
