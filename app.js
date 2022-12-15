57399db74f9132cd13d7de27454572c2fd45fd0b
aguss rodriguez <agussrg80@gmail.com>
2022-12-15 16:59:20 -0300

    add contact section

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const button = document.querySelector(".button");
 const links = document.querySelector(".nav__links");
 
+const form = document.getElementById("form");
+const forSend = document.getElementById("forSend");
+
 button.addEventListener("click", () => {
 	links.classList.toggle("active");
+});
+
+form.addEventListener("submit", (e) => {
+	console.log("hola");
+	e.preventDefault();
 });
